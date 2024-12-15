4fb71ff0ac00f5092d3e34ea60485766329ab1fd
Hussain096 <sebtx096@gmail.com>
2024-12-15 19:09:01 +0500

    done ques 1

--- ques1.dart
+++ ques1.dart
@@ -1,5 +1,5 @@
-import 'dart:io';
-
+// Q.1: Create a list of names and print all names using list.
 void main() {
-
+  List<String> names = ["Bilal", "Kamran", "Hussain", "Ali"];
+  names.forEach((a) => print(a));
 }
